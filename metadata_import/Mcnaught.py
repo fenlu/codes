8810b90a94df2c4a253841c15ac9c943b428a579
fenlu <fenlu@uta.edu>
2021-05-21 17:17:26 -0500

    add metadata_import files

---
+++ metadata_import/Mcnaught.py
@@ -0,0 +1,66 @@
+#!/usr/local/bin/python3
+
+import sys
+import os
+import io
+import pysftp
+from pymarc import MARCReader
+import csv
+from datetime import datetime
+
+def Mcnaugh_sftp(dir,log):
+    #retrieve files from marcive server  
+    with pysftp.Connection('ftp.books.brodart.com/', username='', password='') as sftp:
+        log.write('Connection succesfully stablished ...\n')
+        sftp.cwd('output/ftp/xxxxxxx')
+        list = sftp.listdir()
+        print(list)
+        os.chdir(dir)
+        for i in list:
+            sftp.get(i)
+            log.write("Downloaded file <"+i+">\n")
+        sftp.close()
+        log.write("Close the SFTP connection!\n")
+
+def main():
+    # receipients of processing results
+    # mail_rcp = 'fenlu@uta.edu'
+    
+    # get the current date and time
+    now = datetime.now()
+    date = now.strftime("%m%d%Y")
+
+    downloadDir = '/Mcnaught/download'
+    processedDir = 'Mcnaught/processed'
+    logDir = '/Mcnaught/logs'
+    fileName = "Mcnaught_file_"+date+".csv"
+    logFileName = "Mcnaught_log_"+date+".log"
+ 
+    log = open(f"{logDir}/{logFileName}", 'w', encoding='utf-8')
+    csvfile = open(f"{processedDir}/{fileName}", 'w', encoding='utf-8')
+
+    Mcnaugh_sftp(dir=downloadDir, log=log)
+    f = csv.writer(csvfile)
+    f.writerow(["Title"])
+    for i in os.listdir(downloadDir):
+        if "G127" in i:
+            os.chdir(downloadDir)
+            log.write("Start to process the file <"+i+">\n")
+            counter = 0
+            with open(i,'rb') as fh:
+                counter = counter +1
+                reader = MARCReader (fh)
+                for record in reader:
+                    #print(record)
+                    if record['245'] is not None:
+                        field245 = record['245']['a']
+                    else:
+                        field245 = ""
+                    f.writerow([field245])
+                
+            log.write("Finished processing file<"+i+"> There are "+str(counter)+" records. \n")
+        os.system(f'mv {downloadDir}/{i} {processedDir}/{i}')
+    csvfile.close()
+
+if __name__ == "__main__":
+    main()
